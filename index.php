4525d450330bcb9c291da04b585e6b3968bf6781
singleston <inglestonsam@gmail.com>
2017-08-16 23:13:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,16 @@
     $from = 'From: TangledDemo'; 
     $to = 'inglestonsam@gmail.com'; 
     $subject = 'Hello';
-    $human = $_POST['human'];
 			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 				
-    if ($_POST['submit'] && $human == '4') {				 
+    if ($_POST['submit'] {				 
         if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
 	} 
-    } else if ($_POST['submit'] && $human != '4') {
+    } else if ($_POST['submit']) {
 	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
 ?>
